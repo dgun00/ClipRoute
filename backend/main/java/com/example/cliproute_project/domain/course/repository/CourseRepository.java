7c1103ae43d2ba53e06eb93992b3ad695ef65887
한동건 <127089776+dgun00@users.noreply.github.com>
2026-02-03 21:03:41 +0900

    Revert "FEAT:API 5번 까지 업로드"

--- backend/main/java/com/example/cliproute_project/domain/course/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cliproute_project.domain.course.repository;
-
-import com.example.cliproute_project.domain.course.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course,Long>, CourseRepositoryCustom  {
-
-
-}
--- backend/main/java/com/example/cliproute_project/domain/region/service/query/RegionQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.cliproute_project.domain.region.service.query;
-
-import com.example.cliproute_project.domain.region.dto.res.RegionResDTO;
-
-public interface RegionQueryService {
-
-    // 1번 API: 여행지 리스트 조회
-    RegionResDTO.RegionSimpleListDTO getRegionSimpleList();
-}
